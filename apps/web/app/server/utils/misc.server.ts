3a7585713f5174dd895cc7e0e04a7e422a1fbf08
jarvis394 <tarnatovski@gmail.com>
2024-12-24 21:54:10 +0300

    Remove import of adsress package in misc.server.ts

--- apps/web/app/server/utils/misc.server.ts
+++ apps/web/app/server/utils/misc.server.ts
@@ -1,16 +1,9 @@
-import { ip as ipAddress } from 'address'
-
 export const getHostAdress = () => {
   if (process.env.NODE_ENV === 'development') {
     return 'http://localhost:' + process.env.WEB_PORT
   }
 
-  if (process.env.HOST) {
-    return process.env.HOST
-  }
-
-  const ip = ipAddress()
-  return 'http://' + ip + ':' + process.env.WEB_PORT
+  return process.env.HOST
 }
 
 export const randomString = (length: number) => {
